21bf09a3953e2fbccd825b5d01bf367fadf534f2
paco-pinot <pinot1207@gmail.com>
2022-12-14 14:44:07 +0100

    navbar plus au moins faites

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+$('.dropdown').mouseenter(function(){
+    if(!$('.navbar-toggle').is(':visible')) { // disable for mobile view
+        if(!$(this).hasClass('open')) { // Keeps it open when hover it again
+            $('.dropdown-toggle', this).trigger('click');
+        }
+    }
+});
